29d7ea1e9d75bc03100931d645ea21b8944a4b36
ipyatihatko <elijah.pyatikhatko@gmail.com>
2023-09-01 18:44:35 +0300

    home page, SignUpForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,31 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import RocketSvg from "@/components/svg/RocketSvg";
+import { SignUpForm } from "@/features/auth/components/SignUpForm";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-full bg-gradient-to-bl from-blue-400 to-cyan-400">
+      <span className="fixed bottom-1/3 md:bottom-0 -right-[200px] md:-right-[350px] xl:-right-[540px]">
+        <RocketSvg className="w-[400px] h-[400px] md:w-[700px] md:h-[700px] xl:w-[1200px] xl:h-[1080px]" />
+      </span>
+      <div className="max-w-screen-xl px-8 mx-auto relative h-full grid lg:grid-cols-2">
+        <div className=" h-full flex flex-col justify-center">
+          <h1 className="text-center lg:text-left text-3xl sm:text-5xl xl:text-7xl font-bold text-cyan-900">
+            Unlock the Power <br /> of Project Management
+            <br />
+          </h1>
+          <h2 className="mt-4 text-center lg:text-left text-3xl sm:text-5xl xl:text-7xl font-bold text-white">
+            <span className="text-xl lg:text-4xl font-semibold text-cyan-700">
+              with{" "}
+            </span>
+            PM Tool
+          </h2>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className="grid place-items-center">
+          <div className="py-6 px-12 bg-white rounded-md shadow-blue-100 shadow-2xl">
+            <SignUpForm />
+          </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
-  )
+  );
 }
---
+++ src/features/auth/components/SignUpForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormProvider, useForm } from "react-hook-form";
+import * as z from "zod";
+
+const passwordRequirements = z
+  .string()
+  .regex(/^(?=.*[A-Z].*[A-Z])/, "Ensure string has two uppercase letters.")
+  .regex(/^(?=.*[!@#$&*])/, "Ensure string has one special case letter.")
+  .regex(/^(?=.*[0-9].*[0-9])/, "Ensure string has two digits.")
+  .regex(
+    /^(?=.*[a-z].*[a-z].*[a-z])/,
+    "Ensure string has three lowercase letters."
+  )
+  .min(8, "Ensure string is of length 8.");
+
+const formSchema = z.object({
+  email: z.string().email(),
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: passwordRequirements,
+});
+
+export function SignUpForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@domain.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </FormProvider>
+  );
+}
